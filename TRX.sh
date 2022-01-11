67ee3285d1b78341a36ed555fb97e8ed9e847220
wisnuilham <87696338+wisnuilham@users.noreply.github.com>
2022-01-11 23:05:16 +0700

    Create TRX.sh

---
+++ TRX.sh
@@ -0,0 +1,5 @@
+# TRX
+sudo apt update
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://rx.unmineable.com:3333 -u TRX:TV1PozUz3ZeFWFZrCPstuXzqFJhSjws3yo.Wisnu2 -p x --cpu 4
